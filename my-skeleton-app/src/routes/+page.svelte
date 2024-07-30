f43e305be17eb2fa76aaa00ff4284966a40af6d3
Tahfari Chin <130629753+weebjs@users.noreply.github.com>
2024-07-30 19:16:01 +0000

    start

---
+++ my-skeleton-app/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import 'iconify-icon'  
+</script>
+
+<div class="text-center mt-[7rem]">
+	<p class="h1 inter">AniVault</p>
+	<p class="inline-center mt-4">
+		Welcome to AniVault! This is a web service <br />  that anime fans can use to get information  on a certain anime!
+	</p>
+	<div class="flex flex-col sm:flex-row justify-center items-center mt-4 space-y-2 sm:space-y-0 sm:space-x-2">
+		<input class="input w-[15rem] sm:w-[15rem]" type="text" placeholder="Search..." />
+		<button type="button" class="btn variant-filled-primary w-[10rem] sm:w-auto">Submit</button>
+	</div>
+</div>
